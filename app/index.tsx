69b548afca68c191675649b756d26ab274ba7436
siraajul <shirajulislamparvez@gmail.com>
2024-12-08 01:07:07 +0600

    Link: new screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Not</Text>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -15,7 +19,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
   text: {
-    color: "#ffffff",
+    color: "#fff",
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
   },
 });
