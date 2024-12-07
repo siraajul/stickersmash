4d3b85f53ce6f5b3ce2d764a2a4d9ba2bea59fda
siraajul <shirajulislamparvez@gmail.com>
2024-12-08 00:10:51 +0600

    New: About Page

---
+++ app/about.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2529e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "ffffff",
+  },
+});
